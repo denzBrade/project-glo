27b67af5a93af4d20db72779175ce4fb8f463ad1
Denzil Brade <denzbrade@gmail.com>
2016-12-12 16:24:52 +0000

    React Router included in project

--- project-glo/src/index.js
+++ project-glo/src/index.js
@@ -2,17 +2,23 @@
 * @Author: Denzil Brade <dbrade>
 * @Date:   2016-12-06T16:27:27+00:00
 * @Last modified by:   dbrade
-* @Last modified time: 2016-12-12T15:38:07+00:00
+* @Last modified time: 2016-12-12T16:24:09+00:00
 */
 
-
-
+// import react deps
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { render } from 'react-dom';
+import { Router, Route, browserHistory } from 'react-router';
+
+// import components
 import App from './components/App';
+
+// import styles
 import './styles/index.css';
 
-ReactDOM.render(
-  <App />,
-  document.getElementById('root')
-);
+render((
+  <Router history={browserHistory}>
+    <Route path="/" component={App}>
+    </Route>
+  </Router>
+), document.getElementById('root'));
